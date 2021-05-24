8b8d52e6701fa3c249985c884a0f42d8987cce34
Karl Cridland <karlcridland@gmail.com>
2021-05-24 22:10:16 +0100

    Create project class.

---
+++ public/javascript/classes/Project.js
@@ -0,0 +1,21 @@
+class Project{
+    constructor(id,title) {
+        this.id = id;
+        this.title = title;
+        this.display = document.createElement('div');
+        this.createDisplay();
+    }
+
+    // Sets up all features that should be seen on the display for a user.
+
+    createDisplay(){
+
+    }
+
+    // Saves any updates made by me for the project.
+
+    save(){
+
+    }
+
+}
