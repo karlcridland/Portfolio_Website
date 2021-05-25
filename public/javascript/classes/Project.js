d7a3b7afbca9b814ca68de4cf0a96b80e9f6a949
Karl Cridland <karlcridland@gmail.com>
2021-05-25 22:04:42 +0100

    Create a thumbnail element for projects.

--- public/javascript/classes/Project.js
+++ public/javascript/classes/Project.js
@@ -1,16 +1,27 @@
 class Project{
+
     constructor(id,title) {
         this.id = id;
         this.title = title;
         this.display = document.createElement('div');
+        this.display.setAttribute('class','display');
         this.createDisplay();
     }
 
     // Sets up all features that should be seen on the display for a user.
 
     createDisplay(){
+        const project = this;
+        project.display.innerHTML = '';
         this.getData(function (){
-
+            const title = document.createElement('div');
+            title.setAttribute('class','displayTitle');
+            title.textContent = project.title;
+            project.display.appendChild(title);
+            const image = document.createElement('img');
+            image.setAttribute('class','displayImage');
+            image.getImage('projects/thumbnails/'+project.id);
+            project.display.appendChild(image);
         })
     }
 
