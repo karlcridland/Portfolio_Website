3c45bd821a54270347e1b2a97aa27ff746e74ea0
Karl Cridland <karlcridland@gmail.com>
2021-06-03 23:07:26 +0100

    Methods for gallery page added.

--- public/javascript/gallery.js
+++ public/javascript/gallery.js
@@ -1,7 +1,20 @@
 const gallery = document.getElementById('gallery');
+const filter = {};
 
 ref.child('projects/title').once('value',snapshot=>{
     snapshot.forEach(function (project){
         projects.push(new Project(project.key,project.val()));
     })
 })
+
+// Displays all relevant projects to the gallery element after removing all current projects first.
+
+function displayProjects(){
+
+}
+
+// Returns array of all projects that are relevant from the filter dict.
+
+function relevantProjects(){
+
+}
