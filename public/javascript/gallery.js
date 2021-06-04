1ae97b876124e89e75b716183b9405eb23344663
Karl Cridland <karlcridland@gmail.com>
2021-06-04 23:50:54 +0100

    Structure to gallery methods.

--- public/javascript/gallery.js
+++ public/javascript/gallery.js
@@ -5,16 +5,20 @@ ref.child('projects/title').once('value',snapshot=>{
     snapshot.forEach(function (project){
         projects.push(new Project(project.key,project.val()));
     })
+    displayProjects();
 })
 
 // Displays all relevant projects to the gallery element after removing all current projects first.
 
 function displayProjects(){
+    relevantProjects().forEach(function (project){
 
+    })
 }
 
 // Returns array of all projects that are relevant from the filter dict.
 
 function relevantProjects(){
-
+    let projects = [];
+    return projects;
 }
